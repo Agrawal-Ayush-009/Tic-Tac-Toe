172f92616be8d9eb5396cd08df13b020aca3bc9b
Kanak Goel <goelk0308@gmail.com>
2023-06-24 19:24:29 +0530

    splash and animation

--- app/src/main/java/com/example/tic_tac_toe/SplashActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.tic_tac_toe
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        var icon=findViewById<ImageView>(R.id.ic_launcher)
+        icon.alpha=0f;
+        icon.animate().setDuration(1200).alpha(1f).withEndAction{
+            val i= Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
